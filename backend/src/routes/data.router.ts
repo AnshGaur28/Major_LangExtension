ae80303efa5b23d9645b494e904a213f899ec51f
Ansh <ansh28.dinesh30@gmail.com>
2024-12-02 20:03:42 +0530

    Added scraper-embedding pipeline

--- backend/src/routes/data.router.ts
+++ backend/src/routes/data.router.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { s3Controller, createEmbeddings } from '../controllers/data.controller';  // Destructure both functions
+const { createEmbeddings  } = require('../controllers/data.controller.js');
+// Destructure both functions
+import multer from 'multer';
+const upload = multer();
+
+
 
 const dataRouter = Router();
 
-// Correct usage of the imported controller functions
-dataRouter.post('/s3Data', s3Controller);
-dataRouter.post('/createEmbeddings', createEmbeddings);
+// dataRouter.post('/s3Data', s3Controller);
+// dataRouter.post('/createEmbeddings', upload.single('file'), createEmbeddings);
+
 
 
 export default dataRouter;
